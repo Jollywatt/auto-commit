b8f3ac44fb00ec2b9adab392f76d599b8a1ddbb7
jollywatt <jo.alex.w@gmail.com>
2025-06-07 16:08:00 +0100

    add verbosity option

--- file-watcher.py
+++ file-watcher.py
@@ -7,26 +7,27 @@ from watchdog.observers import Observer
 class DebouncedHandler(FileSystemEventHandler):
     """
     Watch for file changes, but wait for a warmdown priod
-    for things to settle before triggering the chance handler.
+    for things to settle before triggering the change handler.
     """
     def __init__(self, warmdown=1.0):
         self.warmdown = warmdown
         self.timer = None
         self.lock = threading.Lock()
+        self.verbose = True
 
     def on_any_event(self, event: FileSystemEvent):
         with self.lock:
             if self.timer:
                 self.timer.cancel()
-                print("Reset timer...")
+                if self.verbose: print("Restarted warmdown timer...")
             else:
-                print("Starting warmdown timer...")
+                if self.verbose: print("Starting warmdown timer...")
             self.timer = threading.Timer(self.warmdown, lambda: self.handle_change(event))
             self.timer.start()
 
     def handle_change(self, event):
         self.timer = None
-        print("Change event triggered.")
+        if self.verbose: print("Change event triggered.")
 
 
 def main():
